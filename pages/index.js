9887ba5f9f3272f2e47419185bf0cc0b3f362042
Andy-0414 <pjh8667@naver.com>
2020-06-14 12:22:17 +0900

    Create rp

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Link from "next/link"
+
+class Index extends React.Component {
+	render() {
+		return <div id="app">Hello World!</div>;
+	}
+}
+
+export default Index;
